46155f1eb76a6f2236eaf839af35c6cf8eb9ae2f
FacundoS <agustinsouyaux@gmail.com>
2023-05-23 12:26:58 -0300

    Primer Commit

---
+++ src/main/java/com/example/SpringEjercicio3/SpringEjercicio3Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringEjercicio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEjercicio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEjercicio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringEjercicio3/SpringEjercicio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringEjercicio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEjercicio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
